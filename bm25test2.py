cefad6c931aca2b2d2be24624b96ad43eb4ea404
basilbeirouti <BasilBeirouti@gmail.com>
2015-09-26 21:50:11 -0500

    deleted old eval py files

--- bm25test2.py
+++ bm25test2.py
@@ -1,34 +1,18 @@
 __author__ = 'basil.beirouti'
 
 from BM25.BM25Okapi import DocMatrix, QueryMaster
-from BM25.Deprecated import Bm25Query
-from BM25.Plugins import csv_to_tups, tuples_tse_psums_concat
+from Scheduling import headers, schedule_list, whos_on
+from DBConnect.Queries import last_thousand, lookupdict, build_lookup_dict
 from BM25.TextCleaning import wordslist2string, cleanStringAndLemmatize
 import sys, time
+#
+onshift_list = whos_on(schedule_list)
+results = []
+for el in onshift_list:
+    results.append(last_thousand(str(el[1])))
+
+acc = []
+for el in results:
+    acc = acc + el
 
 
-alldata = csv_to_tups("RawData/sampleNoDialHome.csv")
-
-out = tuples_tse_psums_concat(alldata)
-
-okapi_docmatrix = DocMatrix(out, bm25 = True, ngrams_range = (1,1))
-
-tse_dict = okapi_docmatrix.tse_dict
-
-whoson = list(tse_dict.keys())[0:70]
-
-query_master = QueryMaster(okapi_docmatrix)
-
-toppredictions = query_master.queryalgorithm("disk drive failure", whoson)
-predictednames, predictedscores = zip(*toppredictions)
-predictednames = set(list(predictednames))
-
-temp = Bm25Query(out, ngrams_range = (1,1))
-# test query to make sure everything is working properly
-cleanedquery = wordslist2string(cleanStringAndLemmatize("disk drive failure"))
-tt = temp.queryalgorithm(cleanedquery, 2000)
-
-newels = []
-for el in tt:
-    if el[0] in set(whoson):
-        newels.append(el)
